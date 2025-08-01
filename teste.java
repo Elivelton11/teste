3769e609a0eabc7298c7c73498f7cdf037a2b546
Elivelton11 <eliveltonpadilhaa@hotmail.com>
2025-08-01 15:22:35 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,40 +1,28 @@
-//imports
-import java.io.IOException;
-import java.util.Scanner;
-  
-public class Principal {
-    private static Fibonacci fib;
-    private static Scanner entrada;
-     
-    public static void main ( String args [ ] ) {
-        double num = 0;
-        entrada = new Scanner ( System.in );
-        fib = new Fibonacci ();
-        do {
-            System.out.println ( "Entre com um numero não negativo ou \"-1\" para sair: ");
-            num = entrada.nextDouble();
-            if ( num == -1 )
-                System.exit ( 0 );
-            System.out.println ( "O " + num + “-esimo termo de Fibonacci eh: " + fib.CalcularFibonacci( num ) );
-        } while ( num >= 0 );
-    }
-}
+//imports 
+import java.io.IOException; 
+import java.util.Scanner; 
 
+public class Principal { 
+    //Atributos 
+    private static Scanner entrada; 
 
+    public static void main ( String args [ ] ) { 
+        double num = 0; 
+        entrada = new Scanner ( System.in ); 
 
-//imports
-// Não há
- 
-public class Fibonacci {
-    //Atributos
-    private static int conta_chamada = 0; //conta o número de chamadas recursivas
+        do { 
+            System.out.println ( "Entre com um numero de 4 dígitos ou \"-0001\" para sair: "); 
+            if ( num == -1 ) 
+                System.exit ( 0 ); 
+            num = Double.parseDouble( lerEntrada ( 4 ) ); 
+            System.out.println ( "O numero lido eh: " + num ); 
+        } while ( num >= 0 ); 
+    } 
 
-    public double CalcularFibonacci ( double num ) {
-        conta_chamada++;
-        System.out.println ( "Chamada recursiva nr: " + conta_chamada );
-        if ( num != 2 && num != 1 )
-            return CalcularFibonacci ( num - 1 ) + CalcularFibonacci (num – 2);
-        else
-            return 1;
-    }    
+    private static String lerEntrada ( int tam_entrada ) throws IOException { 
+        String entrada = null; 
+             
+            entrada = new String ( System.in.readNBytes ( tam_entrada ) ); 
+        return entrada; 
+    } 
 }
