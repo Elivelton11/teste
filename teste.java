42c68878fc77f1343b8c9d5bfb6551baf909a3f8
Elivelton11 <eliveltonpadilhaa@hotmail.com>
2025-08-01 15:12:20 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -19,3 +19,22 @@ public class Principal {
         } while ( num >= 0 );
     }
 }
+
+
+
+//imports
+// Não há
+ 
+public class Fibonacci {
+    //Atributos
+    private static int conta_chamada = 0; //conta o número de chamadas recursivas
+
+    public double CalcularFibonacci ( double num ) {
+        conta_chamada++;
+        System.out.println ( "Chamada recursiva nr: " + conta_chamada );
+        if ( num != 2 && num != 1 )
+            return CalcularFibonacci ( num - 1 ) + CalcularFibonacci (num – 2);
+        else
+            return 1;
+    }    
+}
