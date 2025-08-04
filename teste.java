f035f794b61406032e368f9e0d67b9cf242b2b6c
Elivelton11 <eliveltonpadilhaa@hotmail.com>
2025-08-04 09:52:34 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,55 +1,56 @@
-//imports
-import java.util.InputMismatchException;
-import java.util.Scanner;
-  
-public class Principal {
-    private static Scanner entrada;
-    private static long res = 0;
-    private static long fat = 0;
-     
+public class ErroValidacao extends Throwable {
+    ErroValidacao ( String msg_erro ) {
+        super ( msg_erro );
+    }    
+    ErroValidacao ( String msg_erro , Throwable causa ) {        
+        super ( msg_erro , causa );
+    }
+    public void atribuirCausa ( Throwable causa ) {
+        initCause ( causa );
+    }        
+    @Override
+    public String toString ( ) {
+        return "ErroValidacao: " + this.getMessage();
+    }
+}
+
+public class Principal {    
     public static void main ( String args [ ] ) {
-        long num = 0;
-        entrada = new Scanner ( System.in );
-        do {
-            System.out.println ( "Entre com um numero inteiro ou \"-1\" para sair: " );
-            try {
-                num = lerEntrada ( entrada );
-            } catch ( ErroValidacao erro ) {
-                System.out.println ( "Entrada inválida!" );
-                System.out.println ( "Causa: " + erro.getCause ( ) );
-                erro.printStackTrace( System.out );
-                System.exit ( -1 );
-            }
-            if ( num == -1 )
-                System.exit ( 0 );
-            else
-                try {
-                    System.out.println ( "O fatorial de " + num + " eh: " + calcularFatorial( num ) );
-                } catch ( ErroValidacao erro ) {
-                    erro.printStackTrace( System.out );
-                }
-        } while ( num >= 0 );
-    }
-    private static long lerEntrada ( Scanner entrada ) throws ErroValidacao {
+        OperacaoArray calc = new OperacaoArray ();
+        char[] op1 = null;
+        char[] op2 = null;
         try {
-            return entrada.nextLong();
-        } catch ( InputMismatchException erro_entrada ) {
-            ErroValidacao erro = new ErroValidacao ( "A entrada " + entrada.next() + " nao eh um numero inteiro!" );
-            erro.atribuirCausa ( erro_entrada );
-            throw erro;
-        }
+            op1 = new char [Short.MAX_VALUE];
+            op2 = new char [Short.MAX_VALUE];
+        } catch ( OutOfMemoryError e ) {
+            Runtime runtime = Runtime.getRuntime ();
+            System.out.println ( "Memoria insuficiente!" );
+            System.out.println ( "A memória total da MVJ eh " + runtime.totalMemory() + " e o máximo eh " + runtime.maxMemory () );
+            System.out.println ( "Reconfigure a MVJ usando o parametro -Xmx<size>. Você precisa de " + 16*Short.MAX_VALUE + " soh para os vetores."); 
+            System.exit ( -1 );
+        }        
+
+            calc.concatenarArray ( op1 , op2 );
+
+        } 
     }
-    private static long calcularFatorial ( long num ) throws ErroValidacao {
-        if ( num > 0 ) { 
-            res = calcularFatorial ( num - 1 );
-            fat = num * res;
-            if ( ( fat / res ) != num ) {
-                throw new ErroValidacao ( "Overflow!");
-            }
-            else
-                return fat;
-        }
-        else
-            return 1;
-    }   
+}
+
+
+public class OperacaoArray {
+  
+    public char[] concatenarArray ( char[] op1 , char[] op2 ) throws ErroValidacao {
+        int tamnh_res;
+ 
+        tamnh_res = op1.length + op2.length;
+
+        return copiarArray ( op1 , op2 , tamnh_res , op2.length );
+
+    }
+    private char[] copiarArray ( char[] op1 , char[] op2 , int tamnh_res , int n ){
+        char[] resultado = new char [ tamnh_res ];
+        System.arraycopy ( op1 , 0 , resultado , 0 , op1.length );
+        System.arraycopy ( op2 , 0 , resultado , op1.length , n );
+        return resultado;
+    }
 }
